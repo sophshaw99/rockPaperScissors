adcb81d10179b2d25d9f2472aa5a08d9f1971df8
Sophia <sophshaw99@gmail.com>
2017-08-28 11:24:26 -0700

    initial commit

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outcome : UILabel!
+    @IBOutlet weak var challenge: UILabel!
+    
+    let choices = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showResults (randomChoice: String) {
+        let randomIndex = Int(arc4random_uniform(UInt32(3)))
+        let computerChoice = choices[randomIndex]
+        challenge.text = "\(randomChoice) vs \(computerChoice)"
+        if randomChoice == computerChoice {
+            outcome.text = "It's a tie"
+        } else if randomChoice == "Paper" && computerChoice == "Scissors" {
+            outcome.text = "You Lose"
+        } else if randomChoice == "Scissors" && computerChoice == "Rock" {
+            outcome.text = "You Lose"
+        } else if randomChoice == "Rock" && computerChoice == "Paper" {
+            outcome.text = "You Lose"
+        } else {
+            outcome.text = "You Win"
+        }
     }
-
+    
+    @IBAction func rock (_ sender: UIButton){
+       showResults(randomChoice: "Rock")
+    }
+    
+    @IBAction func paper (_ sender: UIButton) {
+       showResults(randomChoice: "Paper")
+    }
+    
+    @IBAction func scissors (_ sender: UIButton) {
+       showResults(randomChoice: "Scissors")
+    }
 
 }
 
